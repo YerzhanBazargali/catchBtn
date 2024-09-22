074776765ecd8f3c97095d0f926ba1ece5080907
YerzhanBazargali <47858555+YerzhanBazargali@users.noreply.github.com>
2024-09-22 16:01:06 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function func(btn){
+  const screenWidth = window.screen.width;
+  const screenHeight = window.screen.height;
+  
+  let xc = Math.floor(Math.random() * screenWidth);
+  let yc = Math.floor(Math.random() * screenHeight);
+  btn.style.top = yc + "px";
+  btn.style.left = xc + "px";
+  
+  //alert(btn.height);
+}
